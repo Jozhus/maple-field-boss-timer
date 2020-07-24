1446a49fc261f1e5c63429a485a098a8de2e8686
Jozhus <vauit3@yahoo.com>
2020-07-23 23:57:05 -0700

    Updates

---
+++ precache-manifest.32a3caac15313d830e3a623aa1e31685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e7a0ad94fa92cc0e402b8a607b7591",
+    "url": "/maple-field-boss-timer/index.html"
+  },
+  {
+    "revision": "9ca243b9a5184496d2ad",
+    "url": "/maple-field-boss-timer/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c4055ea39f12013510a5",
+    "url": "/maple-field-boss-timer/static/css/main.06937d12.chunk.css"
+  },
+  {
+    "revision": "9ca243b9a5184496d2ad",
+    "url": "/maple-field-boss-timer/static/js/2.c4e4cde9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maple-field-boss-timer/static/js/2.c4e4cde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4055ea39f12013510a5",
+    "url": "/maple-field-boss-timer/static/js/main.81173cad.chunk.js"
+  },
+  {
+    "revision": "92c84787d647f982e2bd",
+    "url": "/maple-field-boss-timer/static/js/runtime-main.b24a5012.js"
+  }
+]);
