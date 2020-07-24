da1dfca77a2d1ef7d4c7fb6afc8750ed01fa33b6
Jozhus <vauit3@yahoo.com>
2020-07-23 20:01:34 -0700

    reactapp setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { DataController } from "./Game/DataController";
+import "bootstrap/dist/css/bootstrap.css";
+
+class App extends Component {
+  public render(): JSX.Element {
+    return (
+      <></>
+    );
+  }
+}
+
+export { App };
